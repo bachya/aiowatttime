8fd8f1396254a8e806dee3cbcaa8507d8494da19
Aaron Bach <bachya1208@gmail.com>
2022-05-09 22:14:53 -0600

    Fix

--- noxfile.py
+++ noxfile.py
@@ -25,7 +25,7 @@ def coverage(session: nox.sessions.Session) -> None:
     """Report test coverage."""
     args = session.posargs or [
         "-s",
-        "--cov=aionotion",
+        "--cov=aiowatttime",
         "--cov-report=term-missing",
         "--cov-report=xml",
         "tests/",
